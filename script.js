b32a2e34e6b53a1066a994de507e6002f1734453
Rehan Ahmed <radon420@gmail.com>
2023-06-28 16:26:54 +0500

    update

--- script.js
+++ script.js
@@ -15,10 +15,23 @@ const labelForDay = document.querySelector("#for-day");
 const labelForMonth = document.querySelector("#for-month");
 const labelForYear = document.querySelector("#for-year");
 const inputElementArray = [
+  inputDayElement,
+  inputMonthElement,
+  inputYearElement,
+];
+const labelArray = [labelForDay, labelForMonth, labelForYear];
+const inputInvalidArray = [
   inputInvalidDay,
   inputInvalidMonth,
   inputInvalidYear,
 ];
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const currentMonth = currentDate.getMonth() + 1;
+const currentDay = currentDate.getDate();
+const yearDisplay = document.querySelector(".years");
+const monthDisplay = document.querySelector(".months");
+const dayDisplay = document.querySelector(".days");
 
 // ================
 // Funtions Section
@@ -26,20 +39,32 @@ const inputElementArray = [
 
 function leapYearCheck() {
   let enteredYear = document.querySelector("#year").value;
-  if (/\d+/.test(enteredYear)) {
-    if (enteredYear % 400 == 0) {
-      return 2;
-    } else if (enteredYear % 100 == 0) {
-      return 1;
-    } else if (enteredYear % 4 == 0) {
-      return 2;
-    } else return 1;
-  } else return 0;
+  if (enteredYear % 400 == 0) {
+    return 2;
+  } else if (enteredYear % 100 == 0) {
+    return 1;
+  } else if (enteredYear % 4 == 0) {
+    return 2;
+  } else return 1;
+}
+
+function yearCheck() {
+  let enteredYear = document.querySelector("#year").value;
+
+  if (/\d+/.test(enteredYear) && enteredYear.length == 4) {
+    if (enteredYear > currentYear) {
+      yearErrorToggle(true);
+      inputInvalidYear.textContent = "Must be in the past";
+      return false;
+    } else yearErrorToggle(false);
+    return true;
+  } else yearErrorToggle(true);
+  return false;
 }
 
 function monthCheck() {
   let enteredMonth = document.querySelector("#month").value;
-  if (/\d+/.test(enteredMonth)) {
+  if (/\d+/.test(enteredMonth) && enteredMonth.length == 2) {
     if (thirtyDaysMonth.includes(enteredMonth)) {
       monthErrorToggle(false);
       return 30;
@@ -58,7 +83,7 @@ function monthCheck() {
 function dayCheck() {
   let enteredDay = document.querySelector("#day").value;
 
-  if (/\d+/.test(enteredDay)) {
+  if (/\d+/.test(enteredDay) && enteredDay.length == 2) {
     if (enteredDay < 29 && enteredDay > 0) {
       dayErrorToggle(false);
       return 28;
@@ -110,19 +135,18 @@ function ifNotBlank() {
   let enteredMonth = document.querySelector("#month").value;
   let enteredYear = document.querySelector("#year").value;
   let blankCheckArray = [enteredDay, enteredMonth, enteredYear];
-  let errorToggleFunction = [
-    dayErrorToggle(true),
-    monthErrorToggle(true),
-    yearErrorToggle(true),
-  ];
 
   if (blankCheckArray.every((element) => element.length != 0)) {
     return true;
   } else {
-    for (let i = 0; i <= blankCheckArray.length; i++) {
+    for (let i = 0; i < blankCheckArray.length; i++) {
       if (blankCheckArray[i].length == 0) {
-        errorToggleFunction[i];
-        inputElementArray[i].textContent = "This field is required";
+        if (inputInvalidArray[i].classList.contains("hidden")) {
+          inputElementArray[i].classList.toggle("invalid-input");
+          labelArray[i].classList.toggle("error-text-label");
+          inputInvalidArray[i].classList.toggle("hidden");
+          inputInvalidArray[i].textContent = "This field is required";
+        }
       }
     }
     return false;
@@ -131,73 +155,77 @@ function ifNotBlank() {
 
 function dateFormatCheck() {
   let enteredDay = document.querySelector("#day").value;
-  let enteredMonth = document.querySelector("#month").value;
-  let enteredYear = document.querySelector("#year").value;
   if (ifNotBlank()) {
-    if (dayCheck() != 0 || monthCheck() != 0 || leapYearCheck() != 0) {
-      switch (leapYearCheck()) {
-        case 2:
-          switch (monthCheck()) {
-            case 2:
-              if (enteredDay < 30 && enteredDay > 0) {
-                return true;
-              } else {
-                dayErrorToggle(true);
+    if (yearCheck()) {
+      if (dayCheck() != 0 || monthCheck() != 0) {
+        switch (leapYearCheck()) {
+          case 2:
+            switch (monthCheck()) {
+              case 2:
+                if (enteredDay < 30 && enteredDay > 0) {
+                  return true;
+                } else {
+                  dayErrorToggle(true);
+                  return false;
+                }
+              case 30:
+                if (enteredDay < 31 && enteredDay > 0) {
+                  return true;
+                } else {
+                  dayErrorToggle(true);
+                  return false;
+                }
+              case 31:
+                if (enteredDay < 32 && enteredDay > 0) {
+                  return true;
+                } else {
+                  dayErrorToggle(true);
+                  return false;
+                }
+              case 0:
+                monthErrorToggle(true);
                 return false;
-              }
-            case 30:
-              if (enteredDay < 31 && enteredDay > 0) {
-                return true;
-              } else {
-                dayErrorToggle(true);
+            }
+          case 1:
+            switch (monthCheck()) {
+              case 2:
+                if (enteredDay < 29 && enteredDay > 0) {
+                  return true;
+                } else {
+                  dayErrorToggle(true);
+                  return false;
+                }
+              case 30:
+                if (enteredDay < 31 && enteredDay > 0) {
+                  return true;
+                } else {
+                  dayErrorToggle(true);
+                  return false;
+                }
+              case 31:
+                if (enteredDay < 32 && enteredDay > 0) {
+                  return true;
+                } else {
+                  dayErrorToggle(true);
+                  return false;
+                }
+              case 0:
+                monthErrorToggle(true);
                 return false;
-              }
-            case 31:
-              if (enteredDay < 32 && enteredDay > 0) {
-                return true;
-              } else {
-                dayErrorToggle(true);
-                return false;
-              }
-            case 0:
-              monthErrorToggle(true);
-              return false;
-          }
-        case 1:
-          switch (monthCheck()) {
-            case 2:
-              if (enteredDay < 29 && enteredDay > 0) {
-                return true;
-              } else {
-                dayErrorToggle(true);
-                return false;
-              }
-            case 30:
-              if (enteredDay < 31 && enteredDay > 0) {
-                return true;
-              } else {
-                dayErrorToggle(true);
-                return false;
-              }
-            case 31:
-              if (enteredDay < 32 && enteredDay > 0) {
-                return true;
-              } else {
-                dayErrorToggle(true);
-                return false;
-              }
-            case 0:
-              monthErrorToggle(true);
-              return false;
-          }
-        case 0:
-          yearErrorToggle(true);
-          return false;
+            }
+          case 0:
+            yearErrorToggle(true);
+            return false;
+        }
+      } else {
+        return false;
       }
-    } else yearErrorToggle(true);
-    monthErrorToggle(true);
-    dayErrorToggle(true);
-    return false;
+    } else {
+      yearCheck();
+      dayCheck();
+      monthCheck();
+      return false;
+    }
   } else {
     ifNotBlank();
     return false;
@@ -205,24 +233,19 @@ function dateFormatCheck() {
 }
 
 function calculatedValue() {
-  const currentDate = new Date();
-  const currentYear = currentDate.getFullYear();
-  const currentMonth = currentDate.getMonth() + 1;
-  const currentDay = currentDate.getDate();
   let enteredDay = document.querySelector("#day").value;
   let enteredMonth = document.querySelector("#month").value;
   let enteredYear = document.querySelector("#year").value;
+
   if (dateFormatCheck()) {
-    if (enteredYear <= currentYear) {
-      if (currentMonth > enteredMonth) {
-        console.log(currentYear - enteredYear);
-        console.log(currentMonth - enteredMonth);
-        console.log(currentDay - enteredDay);
-      } else {
-        console.log(currentYear - enteredYear - 1);
-        console.log(currentMonth - enteredMonth + 12);
-        console.log(currentDay - enteredDay);
-      }
-    } else console.log("Past Year");
-  } else console.log("Invalid Date");
+    if (currentMonth > enteredMonth) {
+      yearDisplay.textContent = currentYear - enteredYear;
+      monthDisplay.textContent = currentMonth - enteredMonth;
+      dayDisplay.textContent = currentDay - enteredDay;
+    } else {
+      yearDisplay.textContent = currentYear - enteredYear - 1;
+      monthDisplay.textContent = currentMonth - enteredMonth + 12;
+      dayDisplay.textContent = currentDay - enteredDay;
+    }
+  }
 }
